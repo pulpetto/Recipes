622f86f051ac2eb307f7220ac862afb1ef4a315e
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-07-06 20:07:57 +0200

    ingredients

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ingredient } from '../shared/ingredient.model';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+    selector: 'app-shopping-list',
+    templateUrl: './shopping-list.component.html',
+    styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+    ingredients: ingredient[] = [
+        new ingredient('apples', 5),
+        new ingredient('tomatoes', 10),
+        new ingredient('potatotes', 85),
+    ];
 }
