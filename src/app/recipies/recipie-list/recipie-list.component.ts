fb408269dd2449c75d727d7e06379ee0170e970b
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-07-06 10:57:04 +0200

    showing recipie

--- src/app/recipies/recipie-list/recipie-list.component.ts
+++ src/app/recipies/recipie-list/recipie-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Recipie } from '../recipie.model';
 
 @Component({
-  selector: 'app-recipie-list',
-  templateUrl: './recipie-list.component.html',
-  styleUrls: ['./recipie-list.component.css']
+    selector: 'app-recipie-list',
+    templateUrl: './recipie-list.component.html',
+    styleUrls: ['./recipie-list.component.css'],
 })
 export class RecipieListComponent {
-
+    recipies: Recipie[] = [
+        new Recipie(
+            'A Test Recipie',
+            'Testing',
+            'https://www.maczfit.pl/blog/wp-content/uploads/2021/03/fast_food-960x639.jpeg'
+        ),
+    ];
 }
