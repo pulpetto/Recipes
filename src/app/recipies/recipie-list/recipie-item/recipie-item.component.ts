d9002a543d8011713ce18fdd749367046d213ddf
pulpetto <116948697+pulpetto@users.noreply.github.com>
2023-07-10 17:59:43 +0200

    showing 1 page at the time

--- src/app/recipies/recipie-list/recipie-item/recipie-item.component.ts
+++ src/app/recipies/recipie-list/recipie-item/recipie-item.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Recipie } from '../../recipie.model';
 
 @Component({
-  selector: 'app-recipie-item',
-  templateUrl: './recipie-item.component.html',
-  styleUrls: ['./recipie-item.component.css']
+    selector: 'app-recipie-item',
+    templateUrl: './recipie-item.component.html',
+    styleUrls: ['./recipie-item.component.css'],
 })
 export class RecipieItemComponent {
-
+    recipie: Recipie;
 }
